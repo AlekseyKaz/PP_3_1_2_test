61c7ff0577306fcd8192157e6a804b3af87ff6f0
Алексей <jk4zakov@yandex.ru>
2022-08-31 13:35:10 +0300

    dont transaction

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -11,6 +11,7 @@ import ru.kata.spring.boot_security.demo.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    @Transactional
     @Query("select p from User p join fetch p.roles where p.email = :email")
     User findByEmail(String email);
     void deleteById(long id);
