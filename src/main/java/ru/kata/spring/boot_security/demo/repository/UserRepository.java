06e4d8bd19bc515f2edd8c4d9ac9ff266d869e8a
Алексей <jk4zakov@yandex.ru>
2022-08-31 13:18:04 +0300

    dont transaction

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -1,14 +1,17 @@
 package ru.kata.spring.boot_security.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-
+    @Query("select p from User p join fetch p.roles where p.email = :email")
     User findByEmail(String email);
     void deleteById(long id);
 }
